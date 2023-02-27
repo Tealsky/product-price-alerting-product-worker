1ab7715aaa7cc3045aa4f59cf44c0323b775b58f
Ophélie Adams <ophelieadams@MacBook-Pro-de-Ophelie.local>
2023-02-27 13:29:30 +0100

    install doctrine bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
